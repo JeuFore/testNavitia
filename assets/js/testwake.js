0b2e8af8d5f4b46caad93b8ca1aaf0313e621a8d
Jeufordeo <mat.chast67@hotmail.fr>
2018-10-21 16:50:54 +0200

    Update assets/js/testwake.js

--- assets/js/testwake.js
+++ assets/js/testwake.js
@@ -1,55 +1,12 @@
 function myFunction() {
-	return gladys.location.getUser({id: 1})
-	.then((location) => {			
-		
-		// Position de l'utilisateur
-		from = `${location.longitude};${location.latitude}`;
-		
-		return gladys.calendar.getNextEventUser({id: 1})
-	})
-	.then((event) => {
-	// A mettre un 1 quand test finis
-		if (event.start.getDay() == 0) {
-		
-			return Promise.reject(new Error("Pas d'événement"));
-		}
-			event_date = event.start.getHours()*10000+event.start.getMinutes()*100;
-			loca_event = event.location
-			loca_event = loca_event.replace(/\s/g, "%20").replace(/,/g,"%2C").replace(/é/g,"%C3%A9");
-			console.log(loca_event)
-			
-			// Calcul de la position de l'événement
-			request('http://photon.komoot.de/api/?q='+loca_event, function(err, result, body) {
-				loca_event = JSON.parse(body);
-				to = `${loca_event.features[0].geometry.coordinates[0]};${loca_event.features[0].geometry.coordinates[1]}`;
-				console.log(from)
-				console.log(to)
-				document.getElementById("demo").innerHTML = "coucou1"
-				
-			var url = '@api.navitia.io/v1/journeys?from='
+	request('https://82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car', function(err, response, body) {
+		result = JSON.parse(body);
+		document.getElementById("demo").innerHTML = "Paragraph changed.";
 
-			if (options.coverage) {
-				url = '@api.navitia.io/v1/coverage/'+options.coverage+'/journeys?from='
-			}
-					
-			console.log(url)
-			document.getElementById("demo").innerHTML = "coucou2"
-				// Recherche de l'itinéraire
-				request('https://'+shared.api+url+from+'&to='+to+'&first_section_mode[]='+options.mode+'&min_nb_journeys=10&', function(err, response, body) {
-					result = JSON.parse(body);
-					n = 9;
-					arrival = result.journeys[n].arrival_date_time.split("T")[1];
-					console.log(arrival);
-					while ((arrival > event_date) || (n === 0)){
-					    n = n - 1;
-					    arrival = result.journeys[n].arrival_date_time.split("T")[1];
-					    					}
-					depart = result.journeys[n].departure_date_time.split("T")[1];
-					console.log("Départ à "+depart+" heure");
-					console.log("Arrivée à "+arrival+" heure");
-					document.getElementById("demo").innerHTML = "coucou3"
-					document.getElementById("demo").innerHTML = depart
-			})
-		})
-	})
+		// Affichage du départ et de l'arrivée
+		
+		console.log('depart = '+result.journeys[0].departure_date_time.split("T")[1])
+		   console.log('arrive = '+result.journeys[0].arrival_date_time.split("T")[1])
+	
+		});
  }

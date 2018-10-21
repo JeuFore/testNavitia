bc7b4545f15a69c7b772fda1689b3ea9c4e85d2a
Jeufordeo <mat.chast67@hotmail.fr>
2018-10-21 16:58:12 +0200

    Update assets/js/testwake.js

--- assets/js/testwake.js
+++ assets/js/testwake.js
@@ -2,6 +2,7 @@ var Promise = require('bluebird');
 var request = require('request');
 
 function myFunction() {
+	document.getElementById("demo").innerHTML = "Paragraph changed."
 
 	request('https://82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car', function(err, response, body) {
 		result = JSON.parse(body);

31d47b494dab0167b1453cdf8ea5335d9d6e6a0d
Jeufordeo <mat.chast67@hotmail.fr>
2018-10-23 22:59:40 +0200

    Update assets/js/testwake.js

--- assets/js/testwake.js
+++ assets/js/testwake.js
@@ -8,7 +8,7 @@ function myFunction() {
 }
 document.getElementById("demo").innerHTML = "bruhh"
 
-const theUrl = '82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car'
+const theUrl = 'http://82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car'
 
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request

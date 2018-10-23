cb48b43b3c6aa0d5a3d115dc84657059d3b19bcd
Jeufordeo <mat.chast67@hotmail.fr>
2018-10-23 22:49:32 +0200

    Update assets/js/testwake.js

--- assets/js/testwake.js
+++ assets/js/testwake.js
@@ -8,9 +8,15 @@ function myFunction() {
 }
 document.getElementById("demo").innerHTML = "bruhh"
 
-	request('https://82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car', function(err, response, body) {
-		result = JSON.parse(body);
-		document.getElementById("demo").innerHTML = "Paragraph changed"	
-		});
-		document.getElementById("demo").innerHTML = "akekoukou"	
+const theUrl = 'https://82bbc6b2-cc5d-41ac-8572-9c1bf4e48240@api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car'
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	xmlHttp.send( null );
+	document.getElementById("demo").innerHTML = "Paragraph changed.";
+	return xmlHttp.responseText;
+	
+}
  }

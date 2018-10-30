e14f51233428145bb5d2c187f116ee5755923faf
JeuFore <37927640+JeuFore@users.noreply.github.com>
2018-10-30 19:04:57 +0100

    Update assets/js/testwake.js

--- assets/js/testwake.js
+++ assets/js/testwake.js
@@ -11,7 +11,7 @@ document.getElementById("demo").innerHTML = "bruhh"
 const theUrl = "https://api.navitia.io/v1/journeys?from=2.3083896;48.8660048&to=2.301053;48.881376&first_section_mode[]=car"
 
     var http = new XMLHttpRequest();
-    http.open( "GET", theUrl, true, "82bbc6b2-cc5d-41ac-8572-9c1bf4e48240", "82bbc6b2-cc5d-41ac-8572-9c1bf4e48240"); // false for synchronous request
+    http.open( "GET", theUrl, true, "82bbc6b2-cc5d-41ac-8572-9c1bf4e48240"); // false for synchronous request
 	http.send();
 	http.onload = () => document.getElementById("demo").innerHTML = http.responseText;
 	return http.responseText;

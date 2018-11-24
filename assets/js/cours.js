98c7b04106c554fd1979974904755a4baaa6a91e
JeuFore <37927640+JeuFore@users.noreply.github.com>
2018-11-24 17:13:10 +0100

    Update cours.js

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -27,19 +27,13 @@ ajaxGet("https://www.data.gouv.fr/api/1/organizations/premier-ministre/", functi
 });
 
 // Accès à la météo de Lyon avec la clé d'accès 50a65432f17cf542
-ajaxGet("http://api.wunderground.com/api/50a65432f17cf542/conditions/q/France/Lyon.json", function (reponse) {
-    var meteo = JSON.parse(reponse);
-    // Récupération de certains résultats
-    var temperature = meteo.current_observation.temp_c;
-    var humidite = meteo.current_observation.relative_humidity;
-    var imageUrl = meteo.current_observation.icon_url;
-    // Affichage des résultats
-    var conditionsElt = document.createElement("div");
-    conditionsElt.textContent = "Il fait actuellement " + temperature +
-        "°C et l'humidité est de " + humidite;
-    var imageElt = document.createElement("img");
-    imageElt.src = imageUrl;
-    var meteoElt = document.getElementById("meteo");
-    meteoElt.appendChild(conditionsElt);
-    meteoElt.appendChild(imageElt);
+ajaxGet("https://api.navitia.io/", function (reponse) {
+    var navitia = JSON.parse(reponse);
+    // Récupération de certains résultat
+    var descriptionElt = document.createElement("p");
+    descriptionElt.textContent = premierMinistre.versions[0].status;
+    var logoElt = document.createElement("img");
+    logoElt.src = premierMinistre.logo;
+    premMinElt.appendChild(descriptionElt);
+    premMinElt.appendChild(logoElt);
 });

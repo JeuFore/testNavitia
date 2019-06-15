3a3350ecc870d5f369d0f1a8195d0b84f68615ff
JeuFore <37927640+JeuFore@users.noreply.github.com>
2019-06-15 22:17:53 +0200

    navitia

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ module.exports = function (sails) {
     var exec = require('./lib/API/exec.js');
     var json = require('./lib/json.js');
     var AutoWakeUp = require('./lib/API/AutoWakeUp.js');
+    var bis = require('./lib/API/bis.js');
 
     gladys.on('ready', function(){
         init().catch(sails.log.warn);
     });
     
     return {
+        bis,
         json,
         exec,
         AutoWakeUp,

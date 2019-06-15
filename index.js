319b4054d7a6180cff6b5692b06d1afc0e37849a
JeuFore <37927640+JeuFore@users.noreply.github.com>
2019-06-15 19:17:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function (sails) {
 
 	var init = require('./lib/init.js');
-    var exec = require('./lib/exec.js');
+    var exec = require('./lib/API/exec.js');
     var json = require('./lib/json.js');
-    var AutoWakeUp = require('./lib/AutoWakeUp.js');
+    var AutoWakeUp = require('./lib/API/AutoWakeUp.js');
 
     gladys.on('ready', function(){
         init().catch(sails.log.warn);
